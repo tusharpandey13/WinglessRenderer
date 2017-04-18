1fc86555e1a58104095191876c13e97fc2e8b59b
Tushar Pandey <tusharpandey13@gmail.com>
2017-04-18 22:33:40 +0530

    Update GameWindow.vb

--- WinglessRenderer/Code/GameWindow.vb
+++ WinglessRenderer/Code/GameWindow.vb
@@ -5,35 +5,40 @@ Imports OpenTK.Input
 
 Friend NotInheritable Class GameWindow
     Inherits OpenTK.GameWindow
-    Private Buffer As Buffers
-    Private shaderProgram As shaderprogram
-    Private vertexArray As VertexArray(Of Vertex)
-    Private projectionMatrix As Matrix4Uniform
-    Friend sat As Uniform1
+    Private Buffer As Buffers 'buffer objects to store vertex and indice data
+    Private shaderProgram As shaderprogram 'this encapsulates and manages all shaders
+    Private vertexArray As VertexArray(Of Vertex) 'another buffer
+    Private projectionMatrix As Matrix4Uniform 'this the default matrix that our shader uses. 
+    Friend sat As Uniform1 'unsused
 
     Public Sub New(w%, h%)
-        MyBase.New(w, h, New GraphicsMode(32, 24, 0, 1), "OpenTK Intro", GameWindowFlags.Default, DisplayDevice.Default, 4, 3, GraphicsContextFlags.ForwardCompatible)
+        'New GraphicsMode(32, 24, 4, 1)  the 3rd param specifyies msaa level. 4 for best results.
+        MyBase.New(w, h, New GraphicsMode(32, 24, 4, 1), "OpenTK Intro", GameWindowFlags.Default, DisplayDevice.Default, 4, 3, GraphicsContextFlags.ForwardCompatible)
         Width = w : Height = h
         Console.WriteLine("Using OpenGL version " & GL.GetString(StringName.Version))
     End Sub
 
     Protected Overrides Sub OnResize(ByVal e As EventArgs)
-        GL.Viewport(0, 0, Width, Height)
+        GL.Viewport(0, 0, Width, Height) 'resize the viewport when window is resized
     End Sub
 
     Protected Overrides Sub OnLoad(ByVal e As EventArgs)
 
         Buffer = New Buffers()
 
-        Dim m As New Model With {.AntiAliased = 0}
-        
+        'in wingless, models can contain multiple polygons and can have their own transformation matrices
+        'models encapsulates all the vertices and their indices
+
+        Dim m As New Model With {.AntiAliased = 1}
+
         m.Add(New Rectangle(0, 0, 100, 100), Color.Yellow)
         m.Add(New CubicBezier(New Point(40, 200), New Point(40, 100 + 4 / 3 * Math.Tan(Math.PI / 2000)), New Point(140 - 4 / 3 * Math.Tan(Math.PI / 2000), 200), New Point(140, 100)), Color.Black)
         m.Add(New Ellipse(400, 300, 100, 100), Color.Black)
 
-            
+
+        'models need to be added to the buffer to be drawn
         Buffer.Add({m})
-            
+
 
         shaderProgram = New shaderprogram(VertexShader, FragShader)
 
@@ -43,15 +48,19 @@ Friend NotInheritable Class GameWindow
                                                  New VertexAttribute("vTexCoord", 2, VertexAttribPointerType.Float, Vertex.Size, 28)
                                                 )
 
+        'this gl command auto creates an orthographic proj. matrix for us
+        'needed for 2d
         projectionMatrix = New Matrix4Uniform("projectionMatrix")
         projectionMatrix.Data = Matrix4.CreateOrthographicOffCenter(0, Width, Height, 0, 0.01, 100) * Matrix4.CreateTranslation(New Vector3(0, 0, 1))
 
         sat = New Uniform1("sat")
         sat.Data = 1.0!
 
+        'buffers are bound to their respective variables
         Buffer.Bind()
         vertexArray.Bind()
 
+        'at last, the data in the buffers is copied to graphics memory
         Buffer.BufferData()
 
     End Sub
@@ -60,20 +69,26 @@ Friend NotInheritable Class GameWindow
     End Sub
 
     Protected Overrides Sub OnRenderFrame(ByVal e As FrameEventArgs)
+
+        'we can change clear color here
         GL.ClearColor(Color4.White)
+        'specify which masks to clear
         GL.Clear(ClearBufferMask.ColorBufferBit Or ClearBufferMask.DepthBufferBit)
         'GL.Enable(EnableCap.DepthTest) don't enable
-        GL.Enable(EnableCap.Blend)
+        GL.Enable(EnableCap.Blend) 'this enables alpha blending
+        'specify the default method for blending colors
+        'dont touch
         GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha)
 
 
 
-        shaderProgram.use()
-        projectionMatrix.Set(shaderProgram)
-        sat.Set(shaderProgram)
+        shaderProgram.use() 'use our shaders
+        projectionMatrix.Set(shaderProgram) 'set the matrix for use
+        sat.Set(shaderProgram) 'unused
 
 
-        Buffer.Draw()
+        Buffer.Draw() 'draw our models
+
 
         ''Reset state For potential further draw calls (Optional, but good practice)
         'GL.BindVertexArray(0)
@@ -90,4 +105,6 @@ Friend NotInheritable Class GameWindow
         If e.Key = Key.Escape Then Close()
     End Sub
 End Class
+
+
 
